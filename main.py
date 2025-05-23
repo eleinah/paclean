1709bfc24b95fa92d465bdbf1199f752d0757fea
Ellie <ellie@lly.dev>
2025-05-23 07:05:56 -0500

    func: get explicit pkgs (user installed)

---
+++ main.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def get_explicit_pkgs():
+    payload = subprocess.run(
+    ["pacman", "-Qeqt"],
+    capture_output=True,
+    text=True
+    )
+
+    payload.check_returncode()
+    return payload.stdout.strip()

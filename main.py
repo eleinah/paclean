e81b11f72798d20462985f860b100594fa9e645f
Ellie <ellie@lly.dev>
2025-05-23 14:44:31 -0500

    refactor: remove unused get_explicit_pkgs function

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-import subprocess
-
-def get_explicit_pkgs():
-    payload = subprocess.run(
-    ["pacman", "-Qeqt"],
-    capture_output=True,
-    text=True
-    )
-
-    payload.check_returncode()
-    return payload.stdout.strip()
-
-
 
 def main():
     pass
-
-
-
 
 if __name__ == "__main__":
     main()

957d2ca5a96421763869c0e724ced2cb15edac42
Ellie <ellie@lly.dev>
2025-05-25 03:47:03 -0500

    feat: add function to clean pacman cache

--- pkg_actions.py
+++ pkg_actions.py
@@ -43,3 +43,24 @@ def get_pkg_size(pkg: str):
     split_return = unsplit_return.split("\t")
     size = split_return[0]
     return size
+
+def rem_cache(amt_kept: str = "0", dry: bool = False):
+    """
+    Clears the pacman cache and keeps the specified amount (str: amt_kept, default=0)
+    """
+    if amt_kept.isnumeric != True:
+        raise Exception("amt_kept must only be numeric")
+    if dry == True:
+        payload = subprocess.run(
+            ["paccache", "-d", "-vu", "-k", amt_kept],
+            capture_output=True,
+            text=True
+        )
+    else:
+        payload = subprocess.run(
+            ["paccache", "-r", "-vu", "-k", amt_kept],
+            capture_output=True,
+            text=True
+        )
+    payload_result = payload.stdout.strip()
+    return payload_result

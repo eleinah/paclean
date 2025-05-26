24fb21501aaf14cc62c273f3f7261406fdbca498
Ellie <ellie@lly.dev>
2025-05-26 18:43:27 -0500

    feat: remove static table, add selection list

--- elements.py
+++ elements.py
@@ -1,23 +1,25 @@
-from rich.table import Table
-from textual.widgets import Static
-
+from textual.widgets import SelectionList, Static
+from textual.widgets.selection_list import Selection
 from pkg_actions import get_explicit_pkgs, get_pkg_size, get_install_date
 
 
-class PackageList(Static):
+class PackageList(SelectionList):
     """Widget that displays a table of packages with sizes and install dates"""
 
-    def on_mount(self: None):
-        pkg_list = get_explicit_pkgs()
-        table = Table("Packages", "Size", "Installed On", expand=True)
-        for pkg in pkg_list:
+    async def on_mount(self) -> None:
+        self.pkg_list = get_explicit_pkgs()
+        selections = []
+
+        for pkg in self.pkg_list:
             size = get_pkg_size(pkg)
             date = get_install_date(pkg)
-            table.add_row(pkg, size, date)
-        self.update(table)
+            display_txt = f"{pkg}  -  {size}  -  {date}"
+            selections.append(Selection(display_txt, pkg))
+
+        self.add_options(selections)
 
 class PackageInfo(Static):
     """Widget that displays information about a selected package"""
 
-    def on_mount(self: None):
-        self.update(PackageList())
+    def on_mount(self) -> None:
+        self.update("Package info goes here")

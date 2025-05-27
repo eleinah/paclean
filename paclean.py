ede535904a3e98d57132bb58eae5c0a0b9d2085d
Ellie <github@lly.email>
2025-05-27 13:27:48 -0500

    refactor: move check for expac to pkg_actions

--- paclean.py
+++ paclean.py
@@ -1,7 +1,7 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Footer
 from elements import PackageList, PackageInfo, RunExitButton
-from sys import exit
+
 
 class PacLean(App):
     CSS_PATH = "layout.tcss"
@@ -15,8 +15,4 @@ class PacLean(App):
         yield Footer()
 
 if __name__ == "__main__":
-    if shutil.which("expac") is not None:
-        PacLean().run()
-    else:
-        print("Please install 'expac' to use this tool")
-        exit(1)
+    PacLean().run()
--- pkg_actions.py
+++ pkg_actions.py
@@ -1,56 +1,62 @@
 import subprocess
+import shutil
+from sys import exit
 
-def get_explicit_pkgs() -> dict:
-    """Returns all explicitly installed packages as individual strings inside a list"""
+if shutil.which("expac") is not None:
+    def get_explicit_pkgs() -> dict:
+        """Returns all explicitly installed packages as individual strings inside a list"""
 
-    process_result = subprocess.run(
-    ["pacman", "-Qeqt"],
-    capture_output=True,
-    text=True,
-    check=True
-    )
-
-    pkgs = process_result.stdout.strip().split("\n")
-    return pkgs
-
-def get_pkg_info(pkg: str) -> str:
-    """Returns the full info of a given package (pkg: str)"""
-
-    process_result = subprocess.run(
-        ["pacman", "-Qi", pkg],
+        process_result = subprocess.run(
+        ["pacman", "-Qeqt"],
         capture_output=True,
         text=True,
         check=True
-    )
+        )
 
-    info = process_result.stdout.strip()
-    return info
+        pkgs = process_result.stdout.strip().split("\n")
+        return pkgs
 
-def get_pkg_size(pkg: str) -> str:
-    """Returns the size of a given package (pkg: str)"""
+    def get_pkg_info(pkg: str) -> str:
+        """Returns the full info of a given package (pkg: str)"""
 
-    process_result = subprocess.run(
-        ["expac", "-H", "M", "%m", pkg],
-        capture_output=True,
-        text=True,
-        check=True
-    )
+        process_result = subprocess.run(
+            ["pacman", "-Qi", pkg],
+            capture_output=True,
+            text=True,
+            check=True
+        )
 
-    size = process_result.stdout.strip()
-    return size
+        info = process_result.stdout.strip()
+        return info
 
-def rem_cache(amt_kept: str = "0") -> str:
-    """Clears the pacman cache and keeps the specified amount (amt_kept: str, default="0")"""
+    def get_pkg_size(pkg: str) -> str:
+        """Returns the size of a given package (pkg: str)"""
 
-    if amt_kept.strip().isnumeric() == True and 0 <= int(amt_kept) <= 9223372036854775807:
-        pass
-    else:
-        raise Exception("amt_kept must only be numeric and 0-9223372036854775807")
-    process_result = subprocess.run(
-        ["paccache", "-d", "-vu", "-k", amt_kept],
-        capture_output=True,
-        text=True,
-        check=True
-    )
-    output = process_result.stdout.strip()
-    return output
+        process_result = subprocess.run(
+            ["expac", "-H", "M", "%m", pkg],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+
+        size = process_result.stdout.strip()
+        return size
+
+    def rem_cache(amt_kept: str = "0") -> str:
+        """Clears the pacman cache and keeps the specified amount (amt_kept: str, default="0")"""
+
+        if amt_kept.strip().isnumeric() == True and 0 <= int(amt_kept) <= 9223372036854775807:
+            pass
+        else:
+            raise Exception("amt_kept must only be numeric and 0-9223372036854775807")
+        process_result = subprocess.run(
+            ["paccache", "-d", "-vu", "-k", amt_kept],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        output = process_result.stdout.strip()
+        return output
+else:
+    print("Please install 'expac' to use this tool.")
+    exit(1)

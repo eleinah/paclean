8e0bb6a0f1490bfdeaa1d8f577f58ae6e5778635
Ellie <github@lly.email>
2025-05-27 14:42:57 -0500

    feat: implement priv elevation (needed for pacman ops)

--- paclean.py
+++ paclean.py
@@ -1,6 +1,7 @@
 from textual.app import App, ComposeResult
 from elements import PackageList, PackageInfo, RunAndExit
-
+import os
+import sys
 
 class PacLean(App):
     CSS_PATH = "layout.tcss"
@@ -12,5 +13,14 @@ class PacLean(App):
         yield PackageInfo()
         yield RunAndExit()
 
+def elevate():
+    if os.getuid() != 0:
+        cmd = ("sudo", sys.executable, *sys.argv)
+        os.execvp("sudo", cmd)
+
 if __name__ == "__main__":
-    PacLean().run()
+    elevate()
+    if os.geteuid() != 0:
+        exit("PacLean must be run as root to properly access pacman operations.")
+    else:
+        PacLean().run()
